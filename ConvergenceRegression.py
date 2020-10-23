05f9e7343755e1417f05fc935086fc398815b8d5
Julie Butler Hartley <juliehartley1121@gmail.com>
2020-10-22 23:24:09 -0400

    Finished first round of documentation

--- ConvergenceRegression.py
+++ ConvergenceRegression.py
@@ -3,13 +3,12 @@
 # Julie Butler Hartley
 # Date Created: October 22, 2020
 # Last Modified: October 22, 2020
-# Version 1.0.0
+# Version 1.5.0
 #
 # Extrapolates a data set to a converged value using either linear regression, ridge 
 # regresion, or kernel ridge regression.  Take a list of optimal hyperparameters or
 # can perform hyperparameter tuning.
 ########################################
-
 
 #############################
 # IMPORTS
@@ -80,80 +79,116 @@ name, dim, X_tot, y_tot = rs_1_N_26()
 # Using the entire data set as training data for the extrapolation
 X_sequential, y_sequential = time_series_data (y_tot)
 
-
+#############################
+# PERFORM EXTRAPOLATIONS
+#############################
+# If optimal parameters have been provided
 if isOptimalParameters:
+    # If linear regression is wanted
     if isLinearRegression:
+        # Start linear regression instance
         lr = LinearRegressionAnalysis
+        # If extrapolation with autoregression is wanted
         if isAutoRegression: 
             y_return = lr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+        # Otherwise extrapolate without autoregression
         else:
             y_return = lr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+    # If ridge regression is wanted
     if isRidgeRegression:
+        # Start ridge regression instance
         rr = RidgeRegressionAnalysis
+        # If extrapolation with autoregression is wanted
         if isAutoRegression: 
             y_return = rr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+        # Otherwise extrapolate without autoregression
         else:
             y_return = rr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+    # If kernel ridge regression is wanted
     if isKernelRidgeRegression:
+        # Start kernel ridge regression instance
         krr = KernelRidgeRegressionAnalysis
+        # If extrapolation with autoregression is wanted        
         if isAutoRegression: 
             y_return = krr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+        # Otherwise extrapolate without autoregression            
         else:
             y_return = krr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
 else:
+    # Create training data that will be used to find the optimal parameters
+    X_train, y_train = time_series_data(y_tot[:dim])
+    # If linear regression is wanted
     if isLinearRegression:
+        # Start linear regression instance
         lr = LinearRegressionAnalysis
+        # List of all possible hyperparameters
         params_list = [[True, False], [True, False]]
-        params_lr = lr.tune_serial_seq (params_list, X_sequential, y_sequential, dim, y_tot, verbose=True, isReturnBest = False, threshold = 0)
+        # Find the optimized hyperparameter combination using the training data created above
+        params_lr = lr.tune_serial_seq (params_list, X_train, y_train, dim, y_tot, verbose=True, isReturnBest = False, threshold = 0)
         if isAutoRegression: 
-            y_return = lr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params_lr, isConvergence, convergence_threshold, verbose=True)
+        # If extrapolation with autoregression is wanted
+        if isAutoRegression: 
+            y_return = lr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+        # Otherwise extrapolate without autoregression
         else:
-            y_return = lr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params_lr, isConvergence, convergence_threshold, verbose=True)
+            y_return = lr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+    # If ridge regression is wanted
     if isRidgeRegression:
+        # Start ridge regression instance
         rr = RidgeRegressionAnalysis
+        # Values of hyperparameters to be tested
         normalizes = [True, False]
         solvers= ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga']
         alphas = np.logspace(-50, 0, 500)
         params_list = [normalizes, alphas, solvers]
-        params_rr = rr.tune_serial_seq (params_list, X_sequential, y_sequential, dim, y_tot, verbose=True, isReturnBest = False, threshold = 0)
+        # Find the optimized hyperparameter combination using the training data created above     
+        params_rr = rr.tune_serial_seq (params_list, X_train, y_train, dim, y_tot, verbose=True, isReturnBest = False, threshold = 0)
+        # If extrapolation with autoregression is wanted
         if isAutoRegression: 
-            y_return = rr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params_rr, isConvergence, convergence_threshold, verbose=True)
+            y_return = rr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+        # Otherwise extrapolate without autoregression
         else:
-            y_return = rr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params_rr, isConvergence, convergence_threshold, verbose=True)
+            y_return = rr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+    # If kernel ridge regression is wanted
     if isKernelRidgeRegression:
+        # Start kernel ridge regression instance
         krr = KernelRidgeRegressionAnalysis
+        # Values of hyperparameters to be tested        
         kernels = ['polynomial', 'sigmoid']
         degrees = [1, 2, 3, 4]
         alphas = np.logspace(-20, -10, 20)
         coef0s = np.arange(-5, 6, 1)
         gammas = np.logspace(0, 2, 15)
         params_list = [kernels, degrees, alphas, coef0s, gammas]
-        params_krr = krr.tune_serial_seq (params_list, X_sequential, y_sequential, dim, y_tot, verbose=True, isReturnBest = False, threshold = 0)
+        # Find the optimized hyperparameter combination using the training data created above             
+        params_krr = krr.tune_serial_seq (params_list, X_train, y_train, dim, y_tot, verbose=True, isReturnBest = False, threshold = 0)
+        # If extrapolation with autoregression is wanted        
         if isAutoRegression: 
-            y_return = krr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params_krr, isConvergence, convergence_threshold, verbose=True)
+            y_return = krr.unknown_data_cr_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
+        # Otherwise extrapolate without autoregression            
         else:
-            y_return = krr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params_krr, isConvergence, convergence_threshold, verbose=True)
+            y_return = krr.unknown_data_seq (X_sequential, y_sequential, y_tot, total_points, training_dim, params, isConvergence, convergence_threshold, verbose=True)
             print ('CONVERGED??', y_return[-1])
             print ('LENGTH??', len(y_return))
