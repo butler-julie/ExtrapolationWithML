4cfac02a59152a8d781690bb0d2a32964ad8e961
Julie Butler Hartley <juliehartley1121@gmail.com>
2020-10-25 15:27:46 -0400

    Updating internal documentation

--- DataSets/DataSets.py
+++ DataSets/DataSets.py
@@ -3,14 +3,16 @@
 # Julie Hartley
 # Version 1.1.0
 # Created: December 28, 2019
-# Last Modified: December 29, 2019
+# Last Modified: October 25, 2020
 #
 # Collection of data sets to be used in the many-body/machine learning extrapolation 
-# project
+# project.  Also includes some non-physical data sets in the form of polynomials and
+# trigonometric functions.
 #################################################
 
 #################################################
 # OUTLINE:
+# Note that these are not the names of the methods but are descriptive titles
 # Coupled Cluster Data Sets:
 #   Pairing Model:
 #       Vary Dimension
@@ -34,24 +36,30 @@
 #   Arctangent
 #################################################
 
+#################################################
+# Note on the generated function data sets:
+# Unlike the other data sets, these functions accept arguments, which differ
+# per data set and are defined below in the function docstring.  The x range 
+# for the function is from -1 to 1, containing 200 points separated by 0.01.
+# the training dimension is set to be the first 50 points (which would be the 
+# x range between -1 and -0.5).
+#################################################
+
 #############################
 # IMPORTS
 #############################
 # THIRD PARTY IMPORTS
-# for creating ranges
+# for creating ranges and arrays
 import numpy as np
-
-#############################
-# CODES
-#############################
 
 #################################################
 # COUPLED CLUSTER
 #################################################
 
 #############################
-# VARY DIMENSION
+# PAIRING MODEL
 #############################
+# VARYDIMENSION 
 def VaryDimension ():
     """
         Inputs:
@@ -65,20 +73,18 @@ def VaryDimension ():
             X component is the number of particles/holes in the pairing model, y 
             component are the corresponding coupled cluster doubles correlation energies.
             Data set created using the code found at https://nucleartalent.github.io/
-            ManyBody2018/doc/pub/CCM/html/CCM.html.
-            
+            ManyBody2018/doc/pub/CCM/html/CCM.html.     
     """
     data_name ='VaryDimension'
     training_dim = 12
     X_tot = np.arange(2, 42, 2)
-    y_tot = np.array([-0.03077640549, -0.08336233266, -0.1446729567, -0.2116753732, -0.2830637392, -0.3581341341, -0.436462435, -0.5177783846,
-    	-0.6019067271, -0.6887363571, -0.7782028952, -0.8702784034, -0.9649652536, -1.062292565, -1.16231451, 
-    	-1.265109911, -1.370782966, -1.479465113, -1.591317992, -1.70653767])
+    y_tot = np.array([-0.03077640549, -0.08336233266, -0.1446729567, -0.2116753732, -0.2830637392,
+                      -0.3581341341, -0.436462435, -0.5177783846, -0.6019067271, -0.6887363571, 
+                      -0.7782028952, -0.8702784034, -0.9649652536, -1.062292565, -1.16231451, 
+    	              -1.265109911, -1.370782966, -1.479465113, -1.591317992, -1.70653767])
     return data_name, training_dim, X_tot, y_tot 
 
-#############################
-# VARY INTERACTION NEGATIVE
-#############################
+# VARYINTERACTIONNEGATIVE
 def VaryInteractionNegative ():
     """
         Inputs:
@@ -93,8 +99,7 @@ def VaryInteractionNegative ():
             pairing model (only negative values), y component are the corresponding
             coupled cluster doubles correlation energies. Data set created using the
             code found at https://nucleartalent.github.io/ManyBody2018/doc/pub/CCM/html
-            /CCM.html.
-            
+            /CCM.html.     
     """
     data_name='VaryInteractionNegative'
     training_dim = 12
@@ -105,9 +110,7 @@ def VaryInteractionNegative ():
         -0.03406548992,-0.01562553455,-0.004037522178])
     return data_name, training_dim, X_tot, y_tot
 
-#############################
-# VARY INTERACTION POSITIVE
-#############################
+# VARYINTERACTIONPOSITIVE
 def VaryInteractionPositive ():
     """
         Inputs:
@@ -122,8 +125,7 @@ def VaryInteractionPositive ():
             pairing model (only positive values), y component are the corresponding
             coupled cluster doubles correlation energies. Data set created using the
             code found at https://nucleartalent.github.io/ManyBody2018/doc/pub/CCM/html
-            /CCM.html.
-            
+            /CCM.html.     
     """
     data_name = 'VaryInteractionPositive'
     training_dim = 10
@@ -133,9 +135,7 @@ def VaryInteractionPositive ():
         -1.535217909,-2.033720441,-2.80365727,-4.719209688])
     return data_name, training_dim, X_tot, y_tot
 
-#############################
-# Vary Interaction
-#############################
+# VARYINTERACTION
 def VaryInteraction ():
     """
         Inputs:
@@ -150,8 +150,7 @@ def VaryInteraction ():
             pairing model, y component are the corresponding
             coupled cluster doubles correlation energies. Data set created using the
             code found at https://nucleartalent.github.io/ManyBody2018/doc/pub/CCM/html
-            /CCM.html.
-            
+            /CCM.html.      
     """
     data_name='VaryInteractionNegative'
     training_dim = 24
@@ -159,14 +158,16 @@ def VaryInteraction ():
     y_tot = np.array([-1.019822621,-0.9373428759,-0.8571531335,-0.7793624503,-0.7040887974,
         -0.6314601306,-0.561615627,-0.4947071038,-0.4309007163,-0.3703789126,-0.3133427645,
         -0.2600147228,-0.2106419338,-0.1655002064,-0.1248988336,-0.08918647296,-0.05875839719,
-        -0.03406548992,-0.01562553455,-0.004037522178, -0.004334904077,-0.01801896484,-0.04222576507,-0.07838310563,-0.128252924,
+        -0.03406548992,-0.01562553455,-0.004037522178, -0.004334904077,-0.01801896484,-0.04222576507,
+        -0.07838310563,-0.128252924,
         -0.1940453966,-0.2785866456,-0.3855739487,-0.5199809785,-0.6887363571,-0.9019400869,-1.175251697,
         -1.535217909,-2.033720441,-2.80365727,-4.719209688])
     return data_name, training_dim, X_tot, y_tot
 
 #############################
-# INFINITE MATTER DENSITY ENERGY NMAX 20 NUM PART 54
+# INFINITE NUCLEAR MATTER
 #############################
+# INFINITEMATTERDENSITYENERGYNMAX20NUMPART54
 def InfiniteMatterDensityEnergyNmax20NumPart54 ():
     """
     Inputs:
@@ -179,8 +180,7 @@ def InfiniteMatterDensityEnergyNmax20NumPart54 ():
     Origin:
         X component is the density of the particles, y component are the 
         corresponding coupled cluster energies. Data set created using the
-        code found at the Lecture Notes in Physics 936 GitHub.
-            
+        code found at the Lecture Notes in Physics 936 GitHub.     
     """
     data_name = 'InfiniteMatterDensityEnergyNmax20NumPart54'    
     training_dim = 6
@@ -191,10 +191,7 @@ def InfiniteMatterDensityEnergyNmax20NumPart54 ():
 #################################################
 # IN-MEDIUM SIMILARITY RENORMALIZATION GROUP
 #################################################
-
-#############################
-# IMSRG ENERGIES
-#############################
+# IMSRGENERGIES
 def IMSRGEnergies ():
     """
         Inputs:
@@ -207,8 +204,7 @@ def IMSRGEnergies ():
         Origin:
             X component is the strength of the interaction between particles in the 
             pairing model, y component are the corresponding converged IMSRG energy. Data
-            set created using code from Jane Kim.
-            
+            set created using code from Jane Kim.       
     """
     data_name = 'IMSRG'
     training_dim = 12
@@ -220,137 +216,133 @@ def IMSRGEnergies ():
     return data_name, training_dim, X_tot, y_tot
 
 #################################################
-# SIMILARITY RENORMALIZATION GROUP
-#################################################
-
-#############################
-# SRG FULL 
-#############################
-def SRGFull ():
-    """
-        Inputs:
-            None.
-        Returns:
-            data_name (a string): the representative name of the data set
-            training_dim (an int): the suggested training size
-            X_tot (a numpy array): the x component of the data set
-            y_tot (a numpy array): the y component of the data set
-        Origin:
-            X component is SRG flow parameter (s), y component are the Hamiltonians at 
-            that particular evolution of the flow parmater. Data set created using the
-            SRG code written by author, found at ____________________ .
-            
-    """
-    data_name = 'SRGFull'
-    training_dim = 500
-    X_tot = np.arange(0, 4.00, 0.001)
-    y_tot = np.load('H.npy')[0:4000]
-    return data_name, training_dim, X_tot, y_tot
-
-#############################
-# SRG GROUND STATES
-#############################
-def SRGGroundStates ():
-    """
-        Inputs:
-            None.
-        Returns:
-            data_name (a string): the representative name of the data set
-            training_dim (an int): the suggested training size
-            X_tot (a numpy array): the x component of the data set
-            y_tot (a numpy array): the y component of the data set
-        Origin:
-            X component is SRG flow parameter (s), y component are the ground state 
-            energy at that particular evolution of the flow parmater. Data set created 
-            using the SRG code written by author, found at ____________________ .
-            
-    """
-    data_name = 'SRGGroundStates'
-    training_dim = 500
-    X_tot = np.arange(0, 2.00, 0.001)
-    y_tot = np.load('H.npy')[0:2000]
-    y_tot = np.array([y_tot[i][0] for i in range (len(y_tot))])
-    return data_name, training_dim, X_tot, y_tot
-
-
-def TwoDimElectronGasN26 ():
-    """
-    Inputs:
-        None.
-    Returns:
-        data_name (a string): the representative name of the data set
-        training_dim (an int): the suggested training size
-        X_tot (a numpy array): the x component of the data set
-        y_tot (a numpy array): the y component of the data set
-    Origin:
-        X component is SRG flow parameter (s), y component are the ground state 
-        energy at that particular evolution of the flow parmater. Data set created 
-        using the SRG code written by author, found at ____________________ .    
-    """
-    data_name = 'TwoDimElectronGasN26'
-    training_dim = 16
-    X_tot = np.array([6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 50, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300])
-    y_tot = np.array([-0.67039E-01, -0.11728E+00, -0.14399E+00, -0.16185E+00, -0.17787E+00, -0.18554E+00, -0.18859E+00, -0.19101E+00, -0.19336E+00, -0.19458E+00, -0.19616E+00, -0.19721E+00, -0.19776E+00, -0.19876E+00, -0.19913E+00, -0.19959E+00, -0.19988E+00, -0.20039E+00, -0.20068E+00, -0.20153E+00, -0.20229E+00, -0.20309E+00, -0.20354E+00, -0.20381E+00, -0.20400E+00, -0.20414E+00, -0.20424E+00, -0.20431E+00, -0.20437E+00, -0.20442E+00, -0.20447E+00, -0.20450E+00, -0.20453E+00])
-    return data_name, training_dim, X_tot, y_tot
-
-#################################################
 # GENERATED FUNCTION DATA SETS
 #################################################
-
-##############################
 # LINEAR
-##############################
-def linear (scale_factor):
+def linear (scale_factor, intercept):
+    """
+        Inputs:
+            scale_factor (an int or float): the slope of the linear data set
+            intercept (an int or float): the intercept of the linear data set
+        Returns:
+            data_name (a string): the representative name of the data set
+            training_dim (an int): the suggested training size
+            X_tot (a numpy array): the x component of the data set
+            y_tot (a numpy array): the y component of the data set
+        Creates a linear data set that has the following form:
+        y = scale_factor * x + intercept
+    """    
     date_name = 'Linear'
     training_dim = 50
     X_tot = np.arange(-1, 1, 0.01)
-    y_tot = scale_factor*X_tot
+    y_tot = scale_factor*X_tot + intercept
     return data_name, training_dim, X_tot, y_tot
 
-##############################
-# POLYNOMIAL   
-##############################
-def linear (scale_factor, degree):
+# POLYNOMIAL
+def polynomial (scale_factor, constant, degree):
+    """
+        Inputs:
+            scale_factor (an int or float): A constant by which the polynomial data set
+                is scaled
+            constant (an int or float): a constant to be added to the x data
+                before the degree and scale factor are applied
+            degree (an int or float): the degree of the polynomial
+        Returns:
+            data_name (a string): the representative name of the data set
+            training_dim (an int): the suggested training size
+            X_tot (a numpy array): the x component of the data set
+            y_tot (a numpy array): the y component of the data set
+        Creates a linear data set that has the following form:
+        y = scale_factor * (x + constant)^degree
+    """     
     date_name = 'Polynomial'
     training_dim = 50
     X_tot = np.arange(-1, 1, 0.01)
-    y_tot = scale_factor*X_tot**degree
+    y_tot = scale_factor*(X_tot + constant)**degree
     return data_name, training_dim, X_tot, y_tot
 
-##############################
 # SINE
-##############################
-def linear (scale_factor1, scale_factor2):
+def sine (scale_factor1, scale_factor2):
+    """
+        Inputs:
+            scale_factor1 (an int or float): the scale fact to be applied to the data after the
+                sine function is applied        
+            scale_factor2 (an int or float): the scale factor to be applied to the x data before
+                the sine function is applied
+        Returns:
+            data_name (a string): the representative name of the data set
+            training_dim (an int): the suggested training size
+            X_tot (a numpy array): the x component of the data set
+            y_tot (a numpy array): the y component of the data set
+        Creates a linear data set that has the following form:
+        y = scale_factor1 * sin(scalefactor2 * x)
+    """     
     date_name = 'Sine'
     training_dim = 50
     X_tot = np.arange(-1, 1, 0.01)
     y_tot = scale_factor1*np.sin(scale_factor2*X_tot)
     return data_name, training_dim, X_tot, y_tot
     
-##############################
 # COSINE
-##############################
-def linear (scale_factor1, scale_factor2):
+def cosine (scale_factor1, scale_factor2):
+    """
+        Inputs:
+            scale_factor1 (an int or float): the scale fact to be applied to the data after the
+                cosine function is applied        
+            scale_factor2 (an int or float): the scale factor to be applied to the x data before
+                the cosine function is applied
+        Returns:
+            data_name (a string): the representative name of the data set
+            training_dim (an int): the suggested training size
+            X_tot (a numpy array): the x component of the data set
+            y_tot (a numpy array): the y component of the data set
+        Creates a linear data set that has the following form:
+        y = scale_factor1 * cos(scalefactor2 * x)
+    """     
     date_name = 'Cosine'
     training_dim = 50
     X_tot = np.arange(-1, 1, 0.01)
     y_tot = scale_factor1*np.cos(scale_factor2*X_tot)
     return data_name, training_dim, X_tot, y_tot
 
-##############################
-# TANGENT
-##############################
-def linear (scale_factor1, scale_factor2):
+# TAGNGENT
+def tangent (scale_factor1, scale_factor2):
+    """
+        Inputs:
+            scale_factor1 (an int or float): the scale fact to be applied to the data after the
+                tangent function is applied        
+            scale_factor2 (an int or float): the scale factor to be applied to the x data before
+                the tangent function is applied
+        Returns:
+            data_name (a string): the representative name of the data set
+            training_dim (an int): the suggested training size
+            X_tot (a numpy array): the x component of the data set
+            y_tot (a numpy array): the y component of the data set
+        Creates a linear data set that has the following form:
+        y = scale_factor1 * tan(scalefactor2 * x)
+    """     
     date_name = 'Tangent'
     training_dim = 50
     X_tot = np.arange(-1, 1, 0.01)
     y_tot = scale_factor1*np.tan(scale_factor2*X_tot)
     return data_name, training_dim, X_tot, y_tot
 
-##############################
 # ARCTANGENT
-##############################
 def linear (scale_factor1, scale_factor2):
+    """
+        Inputs:
+            scale_factor1 (an int or float): the scale fact to be applied to the data after the
+                arctangent function is applied        
+            scale_factor2 (an int or float): the scale factor to be applied to the x data before
+                the artangent function is applied
+        Returns:
+            data_name (a string): the representative name of the data set
+            training_dim (an int): the suggested training size
+            X_tot (a numpy array): the x component of the data set
+            y_tot (a numpy array): the y component of the data set
+        Creates a linear data set that has the following form:
+        y = scale_factor1 * arctan(scalefactor2 * x)
+    """     
     date_name = 'Arctangent'
     training_dim = 50
     X_tot = np.arange(-1, 1, 0.01)
